3d4b524866b8e45a84c6928ccb632486915b501f
huadjiwisdom23-sys <huadjiwisdom23@gmail.com>
2026-06-23 15:44:37 +0000

    This is the assignment for week 4

---
+++ Myself.java
@@ -0,0 +1,21 @@
+public class Myself {
+    public static void main(String[] args) {
+        // School-related variables string student Name= "huadji wisdom";
+        String CourseName = "Java";
+        int Level = 200;
+        double GPA = 3.56;
+        boolean IsRegistered = true;
+        char GradeCode = 'B';
+        long StudentID = 202423L;
+
+        // Displaying the school information
+        System.out.println("--- Student Profile---");
+        System.out.println("Name:+ StudentName");
+        System.out.println("Course:" + CourseName);
+        System.out.println("Level:" + Level);
+        System.out.println("GPA:" + GPA);
+        System.out.println("Registered:" + IsRegistered);
+        System.out.println("Current Grade:" + GradeCode);
+        System.out.println("Student ID:" + StudentID);
+    }
+}
